430a81ac745c1eac3cf9d39eb2a436f1cdf46572
81120 <nicolas.leo.hu@gmail.com>
2015-08-12 17:54:04 +0800

    add event list

--- www/js/app.js
+++ www/js/app.js
@@ -18,23 +18,96 @@ angular.module('starter', ['ionic','ui.calendar'])
   });
 })
 
-.controller('myController',function($scope){
+.service('Events',function($rootScope){
+  var events=[]
+
+  return {
+    loadEvents:function(){
+      events=[{
+        title:'medicine',
+        start:'2015-08-20'
+      },{
+        title:'examination',
+        start:'2015-08-20'
+      },{
+        title:'cost',
+        start:'2015-08-16'
+      },{
+        title:'examination',
+        start:'2015-08-17'
+      },{
+        title:'cost',
+        start:'2015-08-12'
+      },{
+        title:'examination',
+        start:'2015-08-20'
+      },{
+        title:'examination',
+        start:'2015-08-20'
+      },{
+        title:'examination',
+        start:'2015-08-20'
+      }]
+
+      $rootScope.$broadcast('events_get')
+    },
+
+    getAllEvents:function(){
+      return events
+    },
+
+    getEventsByDate:function(date){
+      return events.filter(function(e){
+        return e.start===date
+      })
+    }
+  }
+})
+
+.controller('myController',function($scope,Events){
+
+  Events.loadEvents()
+
+  var getToday=function(){
+    var today=new Date() 
+    var year=today.getFullYear()
+    var month=today.getMonth()+1
+    var date=today.getDate()
+    return month>10? year+'-'+month+'-'+date:year+'-0'+month+'-'+date
+  }
+
+  $scope.select_date=getToday()
+  var getColorByTitle=function(title){
+    if(title==='medicine'){
+      return 'red'
+    }
+    else if(title==='cost'){
+      return 'yellow'
+    }
+    else if(title==='examination'){
+      return 'green'
+    }
+    else{
+      return 'blue'
+    }
+  }
+  $scope.events_in_select_date=Events.getEventsByDate($scope.select_date)
+
   $scope.eventSources={
-    events:[{
-      title: 'Event1',
-      start: '2015-07-20'
-    },{
-      title: 'Event2',
-      start: '2015-07-22'
-    }],
-    color: 'yellow',  
+    events:Events.getAllEvents().map(function(e){
+      var temp={
+        title:e.title,
+        start:e.start,
+        color:getColorByTitle(e.title)
+      }
+      return temp
+    }),
     textColor: 'black'
   }
 
   $scope.alertEventOnClick=function(date,jsEvent,view){
-    console.log('clicked on:'+date.format())
-    console.log('coordinates:'+jsEvent.pageX+','+jsEvent.pageY)
-    console.log('current views:'+view.name)
+    $scope.select_date=date.format()
+    $scope.events_in_select_date=Events.getEventsByDate($scope.select_date)
   }
 
   $scope.uiConfig = {
@@ -42,13 +115,11 @@ angular.module('starter', ['ionic','ui.calendar'])
        height: 450,
        editable: true,
        header:{
-         left: 'month basicWeek basicDay agendaWeek agendaDay',
+         left: '',
          center: 'title',
          right: 'today prev,next'
        },
        dayClick: $scope.alertEventOnClick,
-       eventDrop: $scope.alertOnDrop,
-       eventResize: $scope.alertOnResize
      }
    }
 })
